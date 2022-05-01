37fc7415cf6f160515df8f89b7839a8dda64ef3b
Sensus0 <alstonpough@gmail.com>
2022-05-01 14:45:48 -0400

    post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
 module.exports = { User, Post };
